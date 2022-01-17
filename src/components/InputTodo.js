ad9150d919bfb73c65ff43760e9a5d73380acf2a
abdessalem1998 <89970442+abdessalem1998@users.noreply.github.com>
2022-01-17 11:58:48 +0100

    add onChang function to handle the input

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -3,12 +3,18 @@ import React, { Component } from 'react';
 
 class InputTodo extends Component {
   state = {
-   title: "";
+    title: ""
+  };
+
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
   };
   render() {
     return (
       <form>
-        <input type="text" placeholder="Add Todo..." />
+        <input type="text" placeholder="Add Todo..." value={this.state.title} name="title" onChange={this.onChange} />
         <button>Submit</button>
       </form>
     )
