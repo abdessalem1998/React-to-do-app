12fa87b3496a7649611431a4db148a87ec2da5cd
abdessalem1998 <89970442+abdessalem1998@users.noreply.github.com>
2022-01-17 12:13:14 +0100

    check for emty strings

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -14,11 +14,16 @@ class InputTodo extends Component {
 
   handleSubmit = e => {
     e.preventDefault();
-    this.props.addTodoProps(this.state.title);
-    this.setState({
-      title: "",
-    });
+    if (this.state.title.trim()) {
+      this.props.addTodoProps(this.state.title)
+      this.setState({
+        title: "",
+      })
+    } else {
+      alert("Please write item");
+    }
   };
+
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
