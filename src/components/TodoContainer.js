e25b1c29cfc1cda1f720ddbabadf84b9bfa2532c
abdessalem1998 <89970442+abdessalem1998@users.noreply.github.com>
2022-01-17 22:05:53 +0100

    use useEffect to store data in local storage

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,12 +1,12 @@
 /* eslint-disable  */
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import Header from "./Header"
 import InputTodo from "./InputTodo"
 import TodosList from "./TodosList"
 import { v4 as uuidv4 } from "uuid"
 
 const TodoContainer = () => {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState(getInitialTodos())
 
   const handleChange = id => {
     setTodos(prevState =>
@@ -49,6 +49,19 @@ const TodoContainer = () => {
       })
     )
   }
+
+  function getInitialTodos() {
+  // getting stored items
+  const temp = localStorage.getItem("todos")
+  const savedTodos = JSON.parse(temp)
+  return savedTodos || []
+}
+
+useEffect(() => {
+  // storing todos items
+  const temp = JSON.stringify(todos)
+  localStorage.setItem("todos", temp)
+}, [todos])
 
   return (
     <div className="container">
