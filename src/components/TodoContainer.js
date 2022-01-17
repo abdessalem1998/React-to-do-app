c368915017623b16c764a8d2d5a837a2ef68c1c8
abdessalem1998 <89970442+abdessalem1998@users.noreply.github.com>
2022-01-17 15:07:45 +0100

    delete all the hardcoded todos data

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -8,23 +8,7 @@ import InputTodo from './InputTodo';
 
 class TodoContainer extends React.Component {
   state = {
-    todos: [
-      {
-        id: uuidv4(),
-        title: 'Setup development environment',
-        completed: true,
-      },
-      {
-        id: uuidv4(),
-        title: 'Develop website and add content',
-        completed: false,
-      },
-      {
-        id: uuidv4(),
-        title: 'Deploy to live server',
-        completed: false,
-      },
-    ],
+    todos: [],
   };
 
   handleChange = id => {
