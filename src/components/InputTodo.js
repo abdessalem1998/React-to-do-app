a83501d8b33d404767f01a1fba86e2dcb5313fd7
abdessalem1998 <89970442+abdessalem1998@users.noreply.github.com>
2022-01-17 11:49:53 +0100

    create InputTodo Component and use it in TodoContainer

---
+++ src/components/InputTodo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+/* eslint-disable  */
+
+class InputTodo extends Component {
+  render() {
+    return (
+      <form>
+        <input type="text" placeholder="Add Todo..." />
+        <button>Submit</button>
+      </form>
+    )
+  }
+}
+export default InputTodo
