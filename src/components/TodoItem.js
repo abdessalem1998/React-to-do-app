a79800840cf757e98d1b6d108e2ea10e47acf032
abdessalem1998 <89970442+abdessalem1998@users.noreply.github.com>
2022-01-17 15:27:52 +0100

    implement The componentWillUnmount() method

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -19,6 +19,10 @@ class TodoItem extends React.Component {
     }
   }
 
+  componentWillUnmount() {
+    console.log("Cleaning up...")
+  }
+
   render() {
     const completedStyle = {
       fontStyle: "italic",
