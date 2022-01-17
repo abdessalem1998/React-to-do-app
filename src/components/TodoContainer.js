c22389a5ab8422b89cd21f8d7d11a3b69f94d70b
abdessalem1998 <89970442+abdessalem1998@users.noreply.github.com>
2022-01-17 08:54:25 +0100

    add state that contain default item of the todo app

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -2,12 +2,34 @@ import React from 'react';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class TodoContainer extends React.Component {
+  // eslint-disable-next-line react/state-in-constructor
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Setup development environment',
+        completed: true,
+      },
+      {
+        id: 2,
+        title: 'Develop website and add content',
+        completed: false,
+      },
+      {
+        id: 3,
+        title: 'Deploy to live server',
+        completed: false,
+      },
+    ],
+  };
+
   render() {
     return (
-      <div>
-        <h1>Hello from Create React App</h1>
-        <p>I am in a React Component!</p>
-      </div>
+      <ul>
+        {this.state.todos.map((todo) => ( // eslint-disable-line
+          <li>{todo.title}</li> // eslint-disable-line
+        ))}
+      </ul>
     );
   }
 }
