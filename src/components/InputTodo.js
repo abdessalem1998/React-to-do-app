1232bc874101a97d58e0b55647c794a019418d4a
abdessalem1998 <89970442+abdessalem1998@users.noreply.github.com>
2022-01-17 15:59:29 +0100

    converting InputTodo written with classes to a function-based. using React Hooks

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,43 +1,36 @@
-import React, { Component } from 'react';
 /* eslint-disable  */
+import React, { useState } from "react"
 
-class InputTodo extends Component {
-  state = {
-    title: ""
-  };
+const InputTodo = props => {
+  const [title, setTitle] = useState("")
 
-  onChange = e => {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    if (this.state.title.trim()) {
-      this.props.addTodoProps(this.state.title)
-      this.setState({
-        title: "",
-      })
-    } else {
-      alert("Please write item");
-    }
-  };
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit} className="form-container">
-        <input
-          type="text"
-          className="input-text"
-          placeholder="Add todo..."
-          value={this.state.title}
-          name="title"
-          onChange={this.onChange}
-        />
-        <button className="input-submit">Submit</button>
-      </form>
-    )
+  const onChange = e => {
+    setTitle(e.target.value)
   }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    if (title.trim()) {
+      props.addTodoProps(title)
+      setTitle("")
+    } else {
+      alert("Please write item")
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="form-container">
+      <input
+        type="text"
+        className="input-text"
+        placeholder="Add todo..."
+        value={title}
+        name="title"
+        onChange={onChange}
+      />
+      <button className="input-submit">Submit</button>
+    </form>
+  )
 }
+
 export default InputTodo
