a817dd4f327960da37d6bef8e5b6df41806f31f2
abdessalem1998 <89970442+abdessalem1998@users.noreply.github.com>
2022-01-18 13:13:22 +0100

    adding an array of objects (containing all your links items)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,18 @@
 import React from "react"
 
 const Navbar = () => {
+  const links = [
+    {
+      id: 1,
+      path: "/",
+      text: "Home",
+    },
+    {
+      id: 2,
+      path: "/about",
+      text: "About",
+    },
+  ]
   return <div>Hello from Navbar</div>
 }
 export default Navbar
